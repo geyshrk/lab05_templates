71a3f13e713a8be8a2d99a51aeee85f5eb4ad195
misha <geyshrk@mail.ru>
2024-11-28 21:04:03 +0300

    Пока ничего не менял

---
+++ src/main/java/ru/itis/dis301/lab05/TemplateHandlerServlet.java
@@ -0,0 +1,44 @@
+package ru.itis.dis301.lab05;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Objects;
+
+@WebServlet("*.thtml")
+public class TemplateHandlerServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        String contextPath = request.getContextPath();
+        String pathInfo = request.getPathInfo();
+        String servletPath = request.getServletPath();
+
+        Iterator<String> iter = request.getAttributeNames().asIterator();
+
+
+        InputStream inputStream = TemplateHandlerServlet
+                .class.getClassLoader().getResourceAsStream(request.getServletPath());
+
+        try {
+            byte[] content = inputStream.readAllBytes();
+
+            String contentTemplate = new String(content);
+
+            // Заменить параметры значениями
+
+            response.getWriter().write(contentTemplate);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
